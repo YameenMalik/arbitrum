7736a730a2e7c92e6e7033efd6aa7fc3a816c90c
YameenMalik <yameen@seedcode.io>
2021-07-08 03:08:22 +0500

    minor updates

--- packages/arb-bridge-eth/scripts/rpctest.ts
+++ packages/arb-bridge-eth/scripts/rpctest.ts
@@ -10,7 +10,9 @@ const provider = new ethers.providers.StaticJsonRpcProvider(url, {
 });
 
 async function main(){
-    console.log(+await provider.getBalance("0x5fEf3e25c2c1c13A16CEb8b3F4199AbbA21b33D6"));
+    console.log("Network:", await provider.getNetwork())
+    console.log("BlockNumber:", +await provider.getBlockNumber())
+    console.log("Balance of 0x5fEf3e25c2c1c13A16CEb8b3F4199AbbA21b33D6:", +await provider.getBalance("0x5fEf3e25c2c1c13A16CEb8b3F4199AbbA21b33D6"));
 }
 
 main();
